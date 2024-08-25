63c6269bc0ef1e408eabcb9267b59bce98108cc1
boralim <boralim.corarl@gmail.com>
2024-08-25 12:53:54 +0700

    commit validate address

--- src/BiiSoft.Application/Sessions/Dto/GetCurrentLoginInformationsOutput.cs
+++ src/BiiSoft.Application/Sessions/Dto/GetCurrentLoginInformationsOutput.cs
@@ -1,11 +1,37 @@
-﻿namespace BiiSoft.Sessions.Dto
+﻿using BiiSoft.Enums;
+using System;
+
+namespace BiiSoft.Sessions.Dto
 {
     public class GetCurrentLoginInformationsOutput
     {
         public ApplicationInfoDto Application { get; set; }
-
         public UserLoginInfoDto User { get; set; }
-
         public TenantLoginInfoDto Tenant { get; set; }
+        public GeneralSettingDto GeneralSetting { get; set; }
+        public AdvanceSettingDto AdvanceSetting { get; set; }
+    }
+
+    public class GeneralSettingDto
+    {
+        public Guid? CountryId { get; set; }
+        public string CountryName { get; set; }
+        public string DefaultTimeZone { get; set; }
+        public long? CurrencyId { get; set; }
+        public string CurrencyCode { get; set; }
+        public DateTime? BusinessStartDate { get; set; }
+
+        public int RoundTotalDigits { get; set; }
+        public int RoundCostDigits { get; set; }
+    }
+
+    public class AdvanceSettingDto
+    {
+        public bool MultiBranchesEnable { get; set; }
+        public bool MultiCurrencyEnable { get; set; }
+        public bool LineDiscountEnable { get; set; }
+        public bool TotalDiscountEnable { get; set; }
+        public bool ClassEnable { get; set; }
+        public AddressLevel ContactAddressLevel { get; set; }
     }
 }
--- src/BiiSoft.Application/Sessions/SessionAppService.cs
+++ src/BiiSoft.Application/Sessions/SessionAppService.cs
@@ -1,12 +1,28 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
 using Abp.Auditing;
+using Abp.Domain.Repositories;
+using BiiSoft.Branches;
 using BiiSoft.Sessions.Dto;
+using Microsoft.EntityFrameworkCore;
 
 namespace BiiSoft.Sessions
 {
     public class SessionAppService : BiiSoftAppServiceBase, ISessionAppService
     {
+        private readonly IRepository<CompanyGeneralSetting, long> _companyGeneralSettingRepository;
+        private readonly IRepository<CompanyAdvanceSetting, long> _companyAdvanceSettingRepository;
+
+        public SessionAppService(
+            IRepository<CompanyGeneralSetting, long> companyGeneralSettingRepository,
+            IRepository<CompanyAdvanceSetting, long> companyAdvanceSettingRepository)
+        {
+            _companyGeneralSettingRepository = companyGeneralSettingRepository;
+            _companyAdvanceSettingRepository = companyAdvanceSettingRepository;
+        }
+
         [DisableAuditing]
         public async Task<GetCurrentLoginInformationsOutput> GetCurrentLoginInformations()
         {
@@ -23,6 +39,37 @@ namespace BiiSoft.Sessions
             if (AbpSession.TenantId.HasValue)
             {
                 output.Tenant = ObjectMapper.Map<TenantLoginInfoDto>(await GetCurrentTenantAsync());
+
+                var isDefaultLanguage = await IsDefaultLagnuageAsync();
+
+                output.GeneralSetting = await _companyGeneralSettingRepository.GetAll()
+                                           .AsNoTracking()
+                                           .Select(s => new GeneralSettingDto
+                                           {
+                                               CountryId = s.CountryId,
+                                               CountryName = !s.CountryId.HasValue ? "" : isDefaultLanguage ? s.Country.Name : s.Country.DisplayName,
+                                               DefaultTimeZone = s.DefaultTimeZone,
+                                               CurrencyId = s.CurrencyId,
+                                               CurrencyCode = s.CurrencyId.HasValue ? s.Currency.Code : "",
+                                               BusinessStartDate = s.BusinessStartDate,
+                                               RoundCostDigits = s.RoundCostDigits,
+                                               RoundTotalDigits = s.RoundTotalDigits
+                                           })
+                                           .FirstAsync();
+
+                output.AdvanceSetting = await _companyAdvanceSettingRepository.GetAll()
+                                           .AsNoTracking()
+                                           .Select(s => new AdvanceSettingDto
+                                           {
+                                               MultiBranchesEnable = s.MultiBranchesEnable,
+                                               MultiCurrencyEnable = s.MultiCurrencyEnable,
+                                               LineDiscountEnable = s.LineDiscountEnable,
+                                               TotalDiscountEnable = s.TotalDiscountEnable,
+                                               ClassEnable = s.ClassEnable,
+                                               ContactAddressLevel = s.ContactAddressLevel
+                                           })
+                                           .FirstAsync();
+
             }
 
             if (AbpSession.UserId.HasValue)
