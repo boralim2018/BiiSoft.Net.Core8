ae03ce34dbb90c27b3a4ebfff606eb9a42c64392
DESKTOP-CGFDT18\Bora <boralim.corarl@gmail.com>
2025-05-11 12:29:49 +0700

    add enum extesions

--- src/BiiSoft.Core/Extensions/AccountTypeExtensions.cs
+++ src/BiiSoft.Core/Extensions/AccountTypeExtensions.cs
@@ -57,16 +57,6 @@ namespace BiiSoft.Extensions
                        .ToList();
         }
 
-        public static string GetName(this Enum enumValue)
-        {
-            // Get the name of the enum (e.g. "CashOnHand")
-            var enumName = enumValue.ToString();
-
-            // Replace underscores with spaces and insert space before capital letters (Pascal Case -> Sentence Case)
-            var formattedName = System.Text.RegularExpressions.Regex.Replace(enumName, @"([a-z])([A-Z])", "$1 $2");
-            return formattedName.Replace("_", " "); // Replace underscores with spaces
-        }
-
         public static string ToIntStr(this SubAccountType type)
         {
             return ((int)type).ToString();
---
+++ src/BiiSoft.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BiiSoft.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetName(this Enum enumValue)
+        {
+            // Get the name of the enum (e.g. "CashOnHand")
+            var enumName = enumValue.ToString();
+
+            // Replace underscores with spaces and insert space before capital letters (Pascal Case -> Sentence Case)
+            var formattedName = System.Text.RegularExpressions.Regex.Replace(enumName, @"([a-z])([A-Z])", "$1 $2");
+            return formattedName.Replace("_", " "); // Replace underscores with spaces
+        }
+    }
+}
